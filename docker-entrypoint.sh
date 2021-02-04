b0682593af93dd8db5babd605a44158176a381d9
Howling <73427651+howling-dot@users.noreply.github.com>
2021-02-04 14:35:21 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-if [ -n "$FTP_LIST" ]; then
-	IFS=';' read -r -a parsed_ftp_list <<< "$FTP_LIST" ; unset IFS
-	for ftp_account in ${parsed_ftp_list[@]}
-	do
-		IFS=':' read -r -a tab <<< "$ftp_account" ; unset IFS
-		ftp_login=${tab[0]}
-		ftp_pass=${tab[1]}
-		CRYPTED_PASSWORD=$(perl -e 'print crypt($ARGV[0], "password")' $ftp_pass)
-		mkdir /FTP/$ftp_login
-		useradd --shell /bin/sh ${USERADD_OPTIONS} -d /FTP/$ftp_login --password $CRYPTED_PASSWORD $ftp_login
-		chown -R $ftp_login:$ftp_login /FTP/$ftp_login
-	done
+if [ -z "$USERS" ]; then
+  USERS="ftp|alpineftp"
 fi
+
+for i in $USERS ; do
+    NAME=$(echo $i | cut -d'|' -f1)
+    PASS=$(echo $i | cut -d'|' -f2)
+  FOLDER=$(echo $i | cut -d'|' -f3)
+
+  if [ -z "$FOLDER" ]; then
+    FOLDER="/FTP/$NAME"
+  fi
+
+
+  echo -e "$PASS\n$PASS" | adduser --home $FOLDER $NAME
+  #mkdir -p $FOLDER
+  chown $NAME:$NAME $FOLDER
+  unset NAME PASS FOLDER
+done
 
 if [ -n "$MASQADDR" ]; then
 	sed -i "s/# MasqueradeAddress/MasqueradeAddress $MASQADDR/" /etc/proftpd/proftpd.conf
